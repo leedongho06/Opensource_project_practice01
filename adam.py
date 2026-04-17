246824a06750756d81ffe7e6f43979abedb005cb
leedongho06 <dlehdgh6550@naver.com>
2026-04-17 11:58:47 +0900

    adam and index

---
+++ adam.py
@@ -0,0 +1,22 @@
+
+def test_sphinx(x):
+    "For the test of sphinx DocString.
+
+    :Param x: a dummy string
+    :type x: str
+    :return: the length of the dummy strings.
+    :rtype: int
+    """
+    print("test_sphinx called")
+    return len(x)
+
+test_sphinx("hello ")
+
+# adan
+print("hello adam")
+print(" hi andam")
+
+print("nice to meet you")
+
+print("something happend")
+# adam end.
